3fc9bdc08c894beba346e1e8e233d376fe2b2bd6
jamiejonescera <152496126+jamiejonescera@users.noreply.github.com>
2025-08-02 17:30:34 +0800

    Updated App.js
    Deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -349,7 +349,7 @@ const AuthProvider = ({ children }) => {
 
 const useAuth = () => useContext(AuthContext);
 // API Service
-const API_BASE = 'http://localhost:8000';
+const API_BASE = 'https://grcal5qmnhiq54qfyc37tzyxe0knzdz.lambda-url.ap-northeast-1.on.aws';
 
 const apiService = {
   // Auth endpoints
